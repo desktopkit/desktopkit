57b098ce4232ae9bcfbd3ef39f6c493f5a48de19
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2022-11-03 22:19:59 +0100

    basedir skeleton

---
+++ kits/basedir/basedir.cpp
@@ -0,0 +1,34 @@
+/*
+#
+# DesktopKit
+# <https://desktopkit.org>
+# <https://github.com/desktopkit>
+#
+# Copyright (c) 2022, Ole-André Rodlie <https://github.com/rodlie>
+# All rights reserved.
+#
+# DesktopKit is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# DesktopKit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+*/
+
+#include "basedir.h"
+
+#include <QString>
+#include <QDebug>
+
+DesktopKit::BaseDir::BaseDir()
+{
+    qDebug() << "org.DesktopKit.BaseDir";
+}
---
+++ kits/basedir/basedir.h
@@ -0,0 +1,40 @@
+/*
+#
+# DesktopKit
+# <https://desktopkit.org>
+# <https://github.com/desktopkit>
+#
+# Copyright (c) 2022, Ole-André Rodlie <https://github.com/rodlie>
+# All rights reserved.
+#
+# DesktopKit is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# DesktopKit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+*/
+
+#ifndef DK_BASEDIR_H
+#define DK_BASEDIR_H
+
+#include <string>
+
+namespace DesktopKit
+{
+    class BaseDir
+    {
+    public:
+        BaseDir();
+    };
+}
+
+#endif // DK_BASEDIR_H
